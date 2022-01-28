63a2c2ff76a6e95571f6e134994b84f225ca6d16
vinicius-p-sousa <vinicius.pereira.d.sousa@gmail.com>
2022-01-27 21:26:26 -0300

    express: arquivos estaticos

---
+++ seçao-10(node)/ex10/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const routes = require('./routes')
+const path = require('path')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(routes)
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+
+app.set('views', path.resolve(__dirname, 'src', 'views'))
+app.set('view engine', 'ejs')
+
+app.listen(3000, () => {
+  console.log('acessar: http://localhost:3000')
+  console.log('servidor está sendo executado na porta 3000')
+})

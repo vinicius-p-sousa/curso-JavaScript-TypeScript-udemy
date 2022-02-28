a4727fc5a5ec0022865562afa4a1468effdb7cfc
vinicius-p-sousa <vinicius.pereira.d.sousa@gmail.com>
2022-02-28 11:49:24 -0300

    projeto api-rest

---
+++ seçao-12(api-rest)/app.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import homeRoutes from './src/routes/homeRoutes';
+import './src/database';
+
+dotenv.config();
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+  }
+}
+
+export default new App().app;

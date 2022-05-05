5266970384cdadc90f931adec84b2dca83ae7e3a
vinicius-p-sousa <vinicius.pereira.d.sousa@gmail.com>
2022-05-05 20:31:28 -0300

    creating alunos routes

--- seçao-13(api-rest)/src/controllers/HomeController.js
+++ seçao-13(api-rest)/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'vincius',
-      sobrenome: 'pereira',
-      email: 'email@.com',
-      idade: 17,
-      peso: 180,
-      altura: 1.76,
-    });
-    res.json(novoAluno);
+    res.json('oi, tudo bem!');
   }
 }
 
---
+++ seçao-13(api-rest)/src/database/migration/20220505230207-mudar-email-aluno-unique.js
@@ -0,0 +1,13 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.changeColumn('alunos', 'email', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('users');
+  },
+};
